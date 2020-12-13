59dfe51ca65f62fe01d2a4cc5dcf74f6b09f125d
Sicong Tang <scalliontang@gmail.com>
2020-12-12 19:58:00 -0500

    Initial commit

---
+++ app/src/main/java/com/sicongtang/tinnews/ui/home/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.sicongtang.tinnews.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sicongtang.tinnews.R;
+import com.sicongtang.tinnews.repository.NewsRepository;
+import com.sicongtang.tinnews.repository.NewsViewModelFactory;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    private HomeViewModel viewModel;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
+                .get(HomeViewModel.class);
+        viewModel.setCountryInput("us");
+        viewModel
+                .getTopHeadlines()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("HomeFragment", newsResponse.toString());
+                            }
+                        });
+
+    }
+}
